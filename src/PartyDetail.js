0a4f5119a7ef9f2acbc23fbbf28d646ebe870398
黃仕翰 <boboboys411@gmail.com>
2023-03-07 15:06:08 +0800

    addMemberPopup by andy

--- src/PartyDetail.js
+++ src/PartyDetail.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Expense from "./Expense";
+import AddMemberPopup from "./AddMemberPopup"
 
 const PartyDetail = (props) =>{
-    const {partyName} = props
+    const {partyName, partyId} = props
   
     return (
       <div className="expenses">
         <h2>Party Name : {partyName}</h2>
         <button>Add A Bill</button>
+        <AddMemberPopup partyId={partyId}/>
         <Expense />
       </div>
     );
