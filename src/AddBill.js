ced994c927336bf8da1d5e087c3d9d17c8ec3b56
JimmyC.WU <jimmy07096@gmail.com>
2023-03-10 17:15:00 +0800

    add button done

--- src/AddBill.js
+++ src/AddBill.js
@@ -8,19 +8,28 @@ const AddBill = () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const getData = localStorage.getItem("token");
+    const token = JSON.parse(getData);
     const [name, setName] = useState('');
     const [totalAmount, setTotalAmount] = useState(0);
     const [expense, setExpense] = useState('');
     const [income,setIncome] = useState('');
-    const [inputList, setInputList] = useState([
+    const [WhoNeedtoPayinputList, setInputList] = useState([
       { name: "", cost: "" },
     ]);
+    const [WhoPayinputlist, setWhoPayList] = useState([{ name: "", cost: "" }]);
     const handldeSubmit = () => {
        const api = `http://192.168.1.150/bill-service/api/bill/add`;
        axios
-         .post(api, {
-
-         })
+         .post(api, 
+        {
+          
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
          .then((res) => {
            console.log(res);
          })
@@ -30,10 +39,22 @@ const AddBill = () => {
          });
 
     }
+    const handleInputChange = (e, index) => {
+      const { name, value } = e.target;
+      const list = [...WhoNeedtoPayinputList];
+      list[index][name] = value;
+      setInputList(list);
+    };
 
     const handleAddClick = () => {
-       setInputList([...inputList, { name: "", cost: "" }]);
+       setInputList([...WhoNeedtoPayinputList, { name: "", cost: "" }]);
     }
+
+    const handleRemoveClick = (index) => {
+      const list = [...WhoNeedtoPayinputList];
+      list.splice(index, 1);
+      setInputList(list);
+    };
     return (
       <div>
         <button className="btn" onClick={handleShow}>
@@ -99,11 +120,39 @@ const AddBill = () => {
                 <div className="label-box">
                   <h3>who needs to pay</h3>
                 </div>
-                <div className="input-area">
-                  <input placeholder="name"></input>
-                  <input placeholder="cost"></input>
-                  <button onClick={handleAddClick}>add</button>
-                </div>
+                {WhoNeedtoPayinputList.map((x, i) => {
+                  return (
+                    <div className="bill-Type-box">
+                      <input
+                        name="name"
+                        value={x.name}
+                        placeholder="Username"
+                        onChange={(e) => handleInputChange(e, i)}
+                      />
+                      <input
+                        name="cost"
+                        value={x.cost}
+                        placeholder="Cost"
+                        onChange={(e) => handleInputChange(e, i)}
+                      />
+                      <div>
+                        {WhoNeedtoPayinputList.length !== 1 && (
+                          <button
+                            className="mr10"
+                            onClick={() => handleRemoveClick(i)}
+                          >
+                            Remove
+                          </button>
+                        )}
+                        {WhoNeedtoPayinputList.length - 1 === i && (
+                          <button type="button" onClick={handleAddClick}>
+                            Add
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
               <div className="input-box">
                 <div className="label-box">
