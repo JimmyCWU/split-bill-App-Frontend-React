3770809e1f54d650fc1bff20e482e66402674660
JimmyC.WU <jimmy07096@gmail.com>
2023-03-13 14:42:51 +0800

    13/3/2023

--- src/AddBill.js
+++ src/AddBill.js
@@ -14,16 +14,26 @@ const AddBill = () => {
     const [totalAmount, setTotalAmount] = useState(0);
     const [expense, setExpense] = useState('');
     const [income,setIncome] = useState('');
+    const [billName, setBillName] = useState('');
+    const [billType, setBillType] = useState('');
     const [WhoNeedtoPayinputList, setInputList] = useState([
       { name: "", cost: "" },
     ]);
+
+    const[WhoPaytheBillInputList, setWhoPaytheBillInputList] = useState([ 
+      { name: "", cost: "" },
+    ]);
+
     const [WhoPayinputlist, setWhoPayList] = useState([{ name: "", cost: "" }]);
     const handldeSubmit = () => {
        const api = `http://192.168.1.150/bill-service/api/bill/add`;
        axios
          .post(api, 
         {
-          
+          billName:billName,
+          totalAmount: totalAmount,
+          memberIdMap:WhoNeedtoPayinputList,
+          billType:billType
         },
         {
           headers: {
@@ -39,6 +49,7 @@ const AddBill = () => {
          });
 
     }
+    ////// below functions are "who need to pay"
     const handleInputChange = (e, index) => {
       const { name, value } = e.target;
       const list = [...WhoNeedtoPayinputList];
@@ -55,6 +66,27 @@ const AddBill = () => {
       list.splice(index, 1);
       setInputList(list);
     };
+/////// below functions are " Who pay the bill"
+    const handleBillInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...WhoPaytheBillInputList];
+        list[index][name] = value;
+        setWhoPaytheBillInputList(list);
+    };
+    const handleBillAddClick = () => {
+       setWhoPaytheBillInputList([...WhoPaytheBillInputList, { name: "", cost: "" }]);
+    }
+
+    const handleBillRemoveClick = (index) => {
+      const list = [...WhoPaytheBillInputList];
+      list.splice(index, 1);
+      setWhoPaytheBillInputList(list);
+    };
+
+  ////below function is "bill type"
+    const handleBillType = (e) => {
+        setBillType(e.target.value);
+    }
     return (
       <div>
         <button className="btn" onClick={handleShow}>
@@ -72,12 +104,13 @@ const AddBill = () => {
                 </div>
                 <div className="input-area">
                   <div className="input-box">
-                    <input name="BillName" placeholder="BillName"></input>
+                    <input name="BillName" placeholder="BillName" onChange={(e) => setBillName(e.target.value)}></input>
                   </div>
                   <div className="input-box">
                     <input
                       name="Total Amount"
                       placeholder="Total Amount"
+                      onChange={(e) => setTotalAmount(e.target.value)}
                     ></input>
                   </div>
                 </div>
@@ -94,6 +127,7 @@ const AddBill = () => {
                       id="AA"
                       value="1"
                       name="billType"
+                      onChange={handleBillType}
                     ></input>
                   </div>
                   <div>
@@ -103,6 +137,7 @@ const AddBill = () => {
                       id="Transfer"
                       value="0"
                       name="billType"
+                      onChange={handleBillType}
                     ></input>
                   </div>
                   <div>
@@ -112,6 +147,7 @@ const AddBill = () => {
                       id="GoDutch"
                       value="2"
                       name="billType"
+                      onChange={handleBillType}
                     ></input>
                   </div>
                 </div>
@@ -158,10 +194,39 @@ const AddBill = () => {
                 <div className="label-box">
                   <h3>who pay the bill</h3>
                 </div>
-                <div className="input-area">
-                  <input placeholder="name"></input>
-                  <input placeholder="cost"></input>
-                  <button>add</button>
+                <div>
+                  {WhoPaytheBillInputList.map((x, i) => {
+                    return (
+                      <div className="bill-Type-box">
+                        <input
+                          name="name"
+                          value={x.name}
+                          placeholder="Username"
+                          onChange={(e) => handleBillInputChange(e, i)}
+                        />
+                        <input
+                          name="cost"
+                          value={x.cost}
+                          placeholder="Cost"
+                          onChange={(e) => handleBillInputChange(e, i)}
+                        />
+                        <div>
+                          {WhoPaytheBillInputList.length !== 1 && (
+                            <button
+                              onClick={() => handleBillRemoveClick(i)}
+                            >
+                              Remove
+                            </button>
+                          )}
+                          {WhoPaytheBillInputList.length - 1 === i && (
+                            <button type="button" onClick={handleBillAddClick}>
+                              Add
+                            </button>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })} 
                 </div>
               </div>
             </div>
